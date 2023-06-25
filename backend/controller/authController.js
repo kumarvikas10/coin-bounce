1f3a01fc9731fc2008cd51054ee0748e4c71d511
vikas <vkvikaskumar413@gmail.com>
2023-06-25 16:04:12 +0530

    resolve error

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,64 +1,65 @@
-const Joi = require('joi');
-const User = require('../models/user');
-const bcrypt = require('bcryptjs');
+const Joi = require("joi");
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
 
 const passwordPattern = /^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,25}$/;
+
 const authController = {
-    async register(req,res,next) {
-        //1. Validate user input
+    async register(req, res, next) {
+        // 1. validate user input
         const userRegisterSchema = Joi.object({
-            username: Joi.string().min(5).max(30).required(),
-            name: Joi.string().max(30).required(),
-            email: Joi.string().email().required(),
-            password: Joi.string().pattern(passwordPattern).required(),
-            confirmPassword: Joi.ref('password')
-        })
-
-        const {error} = userRegisterSchema.validate(req.body);
-        //2. if error in validation -> return error in middleware
-
-        if(error){ //next is used to implement next middleware
+          username: Joi.string().min(5).max(30).required(),
+          name: Joi.string().max(30).required(),
+          email: Joi.string().email().required(),
+          password: Joi.string().pattern(passwordPattern).required(),
+          confirmPassword: Joi.ref("password"),
+        });
+        const { error } = userRegisterSchema.validate(req.body);
+    
+        // 2. if error in validation -> return error via middleware
+        if (error) {
+          return next(error);
+        }
+    
+        // 3. if email or username is already registered -> return an error
+        const { username, name, email, password } = req.body;
+    
+        try {
+          const emailInUse = await User.exists({ email });
+    
+          const usernameInUse = await User.exists({ username });
+    
+          if (emailInUse) {
+            const error = {
+              status: 409,
+              message: "Email already registered, use another email!",
+            };
+    
             return next(error);
-        }
-
-        //3. if email or username is already registered -> return an error
-        const {username, name, email, password} = req.body
-
-        try{
-            const emailInUse = await User.exists({email});
-            const usernameInUse = await User.exists({email});
-
-            if(emailInUse){
-                const error = {
-                    status: 409,
-                    message: 'Email already registered, use another email!'
-                }
-                return next(error);
-            }
-
-            if(usernameInUse){
-                const error = {
-                    status: 409,
-                    message: 'Username not available, choose another username!'
-                }
-                return next(error);
-            }
-        }
-        catch(error){
+          }
+    
+          if (usernameInUse) {
+            const error = {
+              status: 409,
+              message: "Username not available, choose another username!",
+            };
+    
             return next(error);
+          }
+        } catch (error) {
+          return next(error);
         }
+    
         // 4. password hash
-        //123abc -> diffrent code oedfdefzedfs
-        //login -> 123abcd -> oedfdefzedfsaas if both hash not match then return error
         const hashedPassword = await bcrypt.hash(password, 10);
 
         // 5. store user data in db
-        const userToRegister = new user ({
+        const userToRegister = new User ({
             username,
             email,
             name,
-            password: hashedPassword
-        })
+            password: hashedPassword,
+        });
 
         await userToRegister.save();
 
