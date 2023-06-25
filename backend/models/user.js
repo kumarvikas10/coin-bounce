df89c9ec7bee848aca1be7b6a03c8d9b3a232196
vikas <vkvikaskumar413@gmail.com>
2023-06-25 13:38:07 +0530

    add authuenticate controller and error handler

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const {schema} = mongoose;
+const {Schema} = mongoose;
 
 const userSchema = new Schema({
     name: {type: String, required: true},
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dbConnect = require('./database/index');
 const {PORT} = require("./config/index");
-const router = require('./routes/index')
+const router = require('./routes/index');
+const errorHandler = require('./middlewares/errorHandler');
+
 const app = express();
+
+app.use(express.json());
 
 app.use(router);
 
@@ -11,5 +15,7 @@ dbConnect();
 app.get('/', (req,res)=>{
     res.json({msg: '\hello-world'})
 })
+
+app.use(errorHandler); //Always Register at the end of the app
 
 app.listen(PORT, console.log(`Backend is runnng on port: ${PORT}`))
