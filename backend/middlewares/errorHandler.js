c25f1a9515ef9733cee583ef4926f8b860867858
vikas <vkvikaskumar413@gmail.com>
2023-06-25 15:30:22 +0530

    resolve error

--- backend/middlewares/errorHandler.js
+++ backend/middlewares/errorHandler.js
@@ -1,20 +1,19 @@
-const {validationError} = require('joi');
+const {ValidationError} = require('joi');
 
 const errorHandler = (error, req, res, next) => {
-    //default error
+    // default error
     let status = 500;
     let data = {
-        message: 'Internal Sever Error'
+        message: 'Internal Server Error'
     }
 
-    if (error instanceof validationError){
+    if (error instanceof ValidationError){
         status = 401;
-        data.message = error.message;     
+        data.message = error.message;
 
         return res.status(status).json(data);
     }
 
-    //all Error status and message
     if (error.status){
         status = error.status;
     }
