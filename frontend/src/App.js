56063db2b041c9879963ddfab2b5d27794356647
vikas <vkvikaskumar413@gmail.com>
2023-09-10 12:46:59 +0530

    frontend work started

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    <div>
+      <h1>Hello World</h1>
+    </div>
+    <Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
