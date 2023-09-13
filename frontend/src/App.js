aa5d516ae99ccfc38b5a28f62a2d4f5186a0fe14
vikas <vkvikaskumar413@gmail.com>
2023-09-13 22:26:45 +0530

    navbar created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home/Home';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <>
-    <div>
-      <h1>Hello World</h1>
+    <div className={styles.container}>
+      <BrowserRouter>
+      <div className={styles.layout}>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<div className={styles.main}><Home /></div>} />
+          <Route path="crypto" exact element={<div className={styles.main}>Crypto Page</div>} />
+          <Route path="blogs" exact element={<div className={styles.main}>Blogs Page</div>} />
+          <Route path="submit" exact element={<div className={styles.main}>Submit Blog</div>} />
+          <Route path="log-in" exact element={<div className={styles.main}>Log In Page</div>} />
+          <Route path="sign-up" exact element={<div className={styles.main}>Sign Up</div>} />
+        </Routes>
+        <Footer />
+      </div>
+      </BrowserRouter>
     </div>
-    <Navbar></Navbar>
-    </>
   );
 }
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,65 @@
-function Navbar () {
-    return (
-        <h2>Navbar</h2>
-    );
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+function Navbar() {
+  return (
+    <>
+      <nav className={styles.navbar}>
+        <NavLink to="/" className={`${styles.logo} ${styles.inActiveStyle}`}>
+          CoinBounce
+        </NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="crypto"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Cryptocurrencies
+        </NavLink>
+        <NavLink
+          to="blogs"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Blogs
+        </NavLink>
+        <NavLink
+          to="submit"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          Submit a Blog
+        </NavLink>
+        <NavLink
+          to="log-in"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          <button className={styles.loginButton}>Log In</button>
+        </NavLink>
+        <NavLink
+          to="sign-up"
+          className={({ isActive }) =>
+            isActive ? styles.activeStyle : styles.inActiveStyle
+          }
+        >
+          <button className={styles.signupButton}>Sign Up</button>
+        </NavLink>
+      </nav>
+      <div></div>
+    </>
+  );
 }
 
 export default Navbar;
