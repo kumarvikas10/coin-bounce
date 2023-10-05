f94343949e06389977a5e898215c528e0f2ace78
dineshkush <kushwahd18@gmail.com>
2023-10-05 16:11:29 +0530

    add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Error from './pages/Error/Error';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import styles from './App.module.css';
 import Protected from './components/Protected/Protected';
+import Login from './components/Login/Login';
 
 function App() {
   const isAuth = true;
@@ -18,7 +19,7 @@ function App() {
           <Route path="crypto" exact element={<div className={styles.main}>Crypto Page</div>} />
           <Route path="blogs" exact element={<Protected isAuth={isAuth}><div className={styles.main}>Blogs Page</div></Protected>} />
           <Route path="submit" exact element={<Protected isAuth={isAuth}><div className={styles.main}>Submit Blog</div></Protected>} />
-          <Route path="login" exact element={<div className={styles.main}>Log In Page</div>} />
+          <Route path="login" exact element={<div className={styles.main}><Login/></div>} />
           <Route path="signup" exact element={<div className={styles.main}>Sign Up</div>} />
           <Route path="*" element={<div className={styles.main}><Error /></div>} />
         </Routes>
--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,5 +1,27 @@
-function Login(){
+import styles from './Login.module.css';
+import TextInput from '../TextInput/TextInput';
+import loginSchema from '../../schemas/loginSchema';
+import {useFormik} from 'formik';
 
+function Login(){
+    const {values, touched, handleBlur, handleChange, errors} = useFormik({
+        initialValues:{
+            username: '',
+            password: ''
+        },
+        validationSchema: loginSchema
+    })
+    return(
+        <div className={styles.loginWrapper}>
+            <div className={styles.loginHeader}>
+                Log in to your account
+            </div>
+            <TextInput type="text" value={values.username} name="username" onBlur={handleBlur} onChange={handleChange} placeholder="username" error={errors.username && touched.username ? 1: undefined}  errormessage = {errors.username}/>
+            <TextInput type="password" name="password" value={values.password} onBlur={handleBlur} onChange={handleChange} placeholder="password" error={errors.password && touched.password ? 1: undefined} errormessage = {errors.password} />
+            <button className={styles.loginButton}>Log In</button>
+            <span>Don't have an account <button className={styles.createAccount}>Register</button></span>
+        </div>
+    )
 }
 
 export default Login;
