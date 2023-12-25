301cd6b113a20cf3e5cac2e3aab04526ef9e6fe3
dineshkush <kushwahd18@gmail.com>
2023-12-25 13:47:09 +0530

    work on articles api

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,38 @@
+import { useState, useEffect } from "react";
+import { getNews } from "../../api/external";
+import styles from '../Home/Home.module.css';
+
 function Home(){
+    const [articles, setArticles] = useState([]);
+    useEffect(()=>{
+        //IIFE
+        (async function newsApiCall(){
+            const response = await getNews();
+            console.log(response);
+            setArticles(response);
+
+        })();
+
+        //cleanup Function
+        setArticles([]);
+    },[]);
+
     return(
-        <div>Homepage</div>
+        <>
+        <div className={styles.header}>
+            Latest Articles
+        </div>
+        <div className={styles.grid}>
+            {
+                articles.map((article) => (
+                    <div className={styles.card}>
+                        <img src={article.urlToImage} alt="bitcoin_1"/>
+                        <h3>{article.title}</h3>
+                    </div>
+                ))
+            }
+        </div>
+        </>
     )
 }
 
